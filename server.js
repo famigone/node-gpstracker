0a5abf54037963cfd7d2883484a2d32e1de2f0c0
fede <fede.amigone@gmail.com>
2021-04-03 22:37:17 -0300

    asdf

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var gpstracker = require("./lib/server");
+var mqtt = require("mqtt");
+var client = mqtt.connect(
+  "mqtt://127.0.0.1",
+  {}
+);
+
+client.on("connect", function() {
+  console.log("MQTT connected");
+});
 
 var server = gpstracker.create().listen(8090, function() {
   console.log("listening your gps trackers on port", 8090);
@@ -8,6 +17,7 @@ server.trackers.on("connected", function(tracker) {
   console.log("tracker connected with imei:", tracker.imei);
 
   tracker.on("position", function(position) {
+    client.publish(tracker.imei, position.lat + "*" + position.lng);
     console.log(
       "tracker {" + tracker.imei + "}: lat",
       position.lat,
